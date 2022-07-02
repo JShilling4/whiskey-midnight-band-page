d95b6921f64c94aa1133f7d19ef475dfa86dbdd8
Justin Shilling <loveofmusic25@hotmail.com>
2022-07-02 19:10:52 -0400

    add gallery page scaffolding

---
+++ src/views/GalleryView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-wrapper">
+    <PageHeading>Gallery</PageHeading>
+    <h2>COMING SOON</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PageHeading from "@/components/PageHeading.vue";
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin-top: 10rem;
+  text-align: center;
+  color: var(--color-primary);
+}
+</style>
