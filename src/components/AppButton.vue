dd37538a2fec4e955537b502ef3de55971ed9ec1
Justin Shilling <loveofmusic25@hotmail.com>
2022-07-02 19:11:06 -0400

    add button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    :class="[
+      'appButton',
+      {
+        primary: color == 'primary',
+        secondary: color == 'secondary',
+      },
+    ]"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  color?: string;
+}
+withDefaults(defineProps<Props>(), {
+  color: "primary",
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  font-family: Roboto;
+  padding: 1rem;
+  background-color: transparent;
+  border: 2px solid var(--color-secondary);
+  color: var(--color-primary);
+  outline: none;
+  min-width: 15rem;
+  border-radius: 2rem;
+  letter-spacing: 1px;
+  font-size: 1.3rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s, letter-spacing 0.3s;
+
+  @media screen and (max-width: 556px) {
+    font-size: 1rem;
+    min-width: 12rem;
+  }
+
+  &:hover {
+    letter-spacing: 1.4px;
+    background-color: var(--color-secondary);
+    color: var(--color-primary);
+    box-shadow: 1rem 2rem 2rem rgba(0, 0, 0, 0.3);
+  }
+}
+</style>
