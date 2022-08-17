f192c4d388a07b4320b8b4051d8fb9bd78372b6f
Justin Shilling <loveofmusic25@hotmail.com>
2022-08-17 10:44:29 -0400

    add show content to gallery

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -6,28 +6,64 @@
       <div class="row">
         <div class="column">
           <img src="@/assets/images/road.jpg" />
-          <img src="@/assets/images/band-setup.jpg" />
+          <img src="@/assets/images/bar13/081322/bar13-81322-1.jpg" />
+          <img src="@/assets/images/bar13/081322/bar13-81322-4.jpg" />
         </div>
         <div class="column">
+          <img src="@/assets/images/bar13/081322/bar13-81322-2.jpg" />
           <img src="@/assets/images/both-stage1.jpg" />
-          <img
-            src="@/assets/images/bar13-flyer-081322.jpg"
-            alt="Bar13 live show on August 13th, 2022"
-          />
+          <img src="@/assets/images/bar13/081322/bar13-81322-6.jpg" />
         </div>
         <div class="column">
           <img src="@/assets/images/both-guitars-crp.jpg" />
-          <img
-            src="@/assets/images/lakelife-flyer-082722.jpg"
-            alt="Bar13 live show on August 13th, 2022"
-          />
+          <img src="@/assets/images/bar13/081322/bar13-81322-3.jpg" />
+        </div>
+        <div class="column">
+          <img src="@/assets/images/bar13/081322/bar13-81322-7.jpg" />
+          <img src="@/assets/images/bar13/081322/bar13-81322-5.jpg" />
         </div>
       </div>
     </section>
     <section class="videos">
       <h2>Videos</h2>
-      <div>
-        <div style="padding: 56.25% 0 0 0; position: relative">
+      <div class="row">
+        <div class="column">
+          <iframe
+            src="https://www.youtube.com/embed/xL7MGaitW38"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div class="column">
+          <iframe
+            src="https://www.youtube.com/embed/VStyO2zKt_Q"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div class="column">
+          <iframe
+            src="https://www.youtube.com/embed/8p1y4vyc9-o"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div class="column">
+          <iframe
+            src="https://www.youtube.com/embed/KbrpYZJKtyk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <!-- <div style="padding: 56.25% 0 0 0; position: relative">
           <iframe
             src="https://player.vimeo.com/video/729345636?h=e8717a0ae3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
             frameborder="0"
@@ -42,7 +78,7 @@
             "
             title="Rehearsal Medley 1"
           ></iframe>
-        </div>
+        </div> -->
       </div>
     </section>
   </div>
@@ -55,20 +91,44 @@ import PageHeading from "@/components/PageHeading.vue";
 <style lang="scss" scoped>
 .page-wrapper {
   min-height: calc(100vh - 22rem);
+  max-width: 1400px;
+  width: initial;
 }
 
 section {
   padding: 2rem;
   margin-bottom: 2rem;
   &.photos {
+    border: 2px solid var(--color-secondary);
     background-color: var(--banner-color);
     h2 {
       color: var(--color-primary);
     }
   }
   &.videos {
+    border: 2px solid var(--color-secondary);
     background-color: var(--color-primary);
     color: var(--banner-color);
+
+    .column {
+      flex: 33%;
+      max-width: 400px;
+    }
+
+    @media screen and (max-width: 1024px) {
+      .column {
+        flex: 50%;
+        max-width: 50%;
+      }
+    }
+
+    /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 600px) {
+      .column {
+        flex: 100%;
+        max-width: 100%;
+      }
+    }
   }
 }
 h2 {
@@ -93,19 +153,27 @@ h2 {
 
 /* Create four equal columns that sits next to each other */
 .column {
-  flex: 33%;
-  max-width: 33%;
+  flex: 25%;
+  /* max-width: 300px; */
   padding: 0 4px;
 }
 
-.column img {
+.column img,
+.column iframe {
   margin-top: 8px;
   vertical-align: middle;
   width: 100%;
 }
 
+iframe {
+  height: 315px;
+}
+
 /* Responsive layout - makes a two column-layout instead of four columns */
 @media screen and (max-width: 800px) {
+  section {
+    padding: 2rem 1rem;
+  }
   .column {
     flex: 50%;
     max-width: 50%;
